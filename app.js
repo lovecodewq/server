2bff321fe9875a2e3f7a7ab13515b8ca79aa26c0
Wenqiang Li <wenqiangli@autox.ai>
2024-04-19 18:58:46 +0800

    add db

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ require('dotenv').config();
 
 const express = require('express')
 const cloudinary = require('cloudinary')
-
+const bodyParse = require('body-parser')
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 })
 const app = express()
-app.use((request, response)=>{
-    response.json({message: 'Hey! this is liwenqiang'})
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: true}));
+app.get('/', (resquest, response) => {response.json({message: 'Hey! get /'})})
+
+app.post('/upload-image', (request, response) => {
+  const data = {image: request.body.image};
+  cloudinary.uploader.upload(data.image)
+      .then((result) => {response.status(200).send({
+              message: 'success',
+              result,
+            })})
+      .catch((error) => {response.status(500).send({
+               message: 'error',
+               error,
+             })})
 })
 
 module.exports = app;
